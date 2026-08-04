1628c34664e113adf7b790d071fd7e2426d4eb25
Serhiy Onyshchenko <ziohimself@gmail.com>
2026-08-05 00:16:22 +0200

    major: now using the proper whydonate campaign

--- src/data/event.ts
+++ src/data/event.ts
@@ -101,7 +101,7 @@ export const eventDetails = {
     url: "https://hurkit.org/",
   },
   goalEur: 3_000,
-  whydonateShortcode: "nudW7",
+  whydonateShortcode: "KvhGb",
   postEvent: {
     thankYouMessage:
       "Thank you to every runner, supporter, and donor who made Run for Ukraine 2026 possible. Together we powered hope on the front line.",
